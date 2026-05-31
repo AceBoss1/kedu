eae750a5c6cfcbbdc7a3bd67badebc54bd59bafc
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2026-05-31 22:16:16 +0100

    Add Next.js configuration with internationalization

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["ig", "en"],
+    defaultLocale: "ig"
+  }
+};
+module.exports = nextConfig;
